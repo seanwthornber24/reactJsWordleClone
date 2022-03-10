1644f5f23825c430f4be1eeb1d852cf8b4cb7867
Sean Thornber <email@example.com>
2022-03-10 20:55:35 +0000

    added green and yellow letters as well as 'animated' letter input

--- src/App.js
+++ src/App.js
@@ -14,41 +14,78 @@ class App extends React.Component {
       row5: "",
       row6: "",
       currentRow: 1,
-      letterStates: [{backgroundColor: "grey", transition: "1s"}]
+      letterStates: [[{backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}, {backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}, {backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}, {backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}, {backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}],
+                      [{backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}, {backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}, {backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}, {backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}, {backgroundColor: "grey", transition: "1s", border: "4px solid #565656"}]
+                    ]
     };
 
-    this.testFunc = this.testFunc.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.animateLetterInput = this.animateLetterInput.bind(this);
+    this.handleWordEnter = this.handleWordEnter.bind(this);
   }
 
   componentDidMount() {
-    document.body.addEventListener("keydown", this.testFunc);
+    document.body.addEventListener("keydown", this.handleKeyPress);
   }
 
   componentWillUnmount() {
-    document.body.removeEventListener("keydown", this.testFunc);
+    document.body.removeEventListener("keydown", this.handleKeyPress);
   }
 
-  testFunc(e) {
-    console.log(e.key);
+  handleKeyPress(e) {
+    // console.log(e.key);
     let thisRow = "row" + this.state.currentRow;
     let updateObj = {};
-    // let x = this.state.letterStates;
-    // x[0] = {backgroundColor: "green", transition: "1s"};
-    // this.setState({
-    //   letterStates: x
-    // })
     if (e.key.match(/^[A-Za-z]$/) && this.state[thisRow].length < 5) {
       updateObj[thisRow] = this.state[thisRow] + e.key.toUpperCase()
       this.setState(updateObj);
+      this.animateLetterInput(false);
     }
     else if (e.key === "Backspace") {
+      this.animateLetterInput(true);
       updateObj[thisRow] = this.state[thisRow].slice(0, -1);
       this.setState(updateObj);
     }
     else if (e.key === "Enter" && this.state[thisRow].length === 5) {
+      this.handleWordEnter();
       this.setState({
         currentRow: this.state.currentRow + 1
       });
+    }
+  }
+
+  animateLetterInput(isBackspace) {
+    let thisRow = "row" + this.state.currentRow;
+    let x = this.state.letterStates;
+    // console.log(x[this.state.currentRow - 1][this.state[thisRow].length - 1]);
+    if (!isBackspace) {
+      x[this.state.currentRow - 1][this.state[thisRow].length - 1] = {transition: "0.2s", transform: "scale(1.07)", border: "4px solid #565656"};
+    }
+    else {
+      x[this.state.currentRow - 1][this.state[thisRow].length - 1] = {transition: "0.5s", transform: "scale(1)", border: "4px solid #565656"};
+    }
+    this.setState({
+      letterStates: x
+    })
+  }
+
+  handleWordEnter() {
+    let thisRow = "row" + this.state.currentRow;
+    console.log(this.state[thisRow]);
+    let x = this.state.letterStates;
+    for (let i = 0; i < 5; i++) {
+      if (this.state[thisRow][i] == this.state.answer[i]) {
+        x[this.state.currentRow - 1][i] = {transition: "0.5s", transform: "scale(1.07)", border: "4px solid #1a5408", backgroundColor: "#236e0c"};
+        this.setState({
+          letterStates: x
+        })
+      }
+      else if (this.state.answer.includes(this.state[thisRow][i])) {
+        x[this.state.currentRow - 1][i] = {transition: "0.5s", transform: "scale(1.07)", border: "4px solid #ba980d", backgroundColor: "#deb510"};
+        this.setState({
+          letterStates: x
+        })
+      }
     }
   }
 
@@ -62,19 +99,19 @@ class App extends React.Component {
         </div>
         
         <div className="row">
-          <div className="letter-box" style={this.state.letterStates[0]}>{this.state.row1[0]}</div>
-          <div className="letter-box">{this.state.row1[1]}</div>
-          <div className="letter-box">{this.state.row1[2]}</div>
-          <div className="letter-box">{this.state.row1[3]}</div>
-          <div className="letter-box">{this.state.row1[4]}</div>
+          <div className="letter-box" style={this.state.letterStates[0][0]}>{this.state.row1[0]}</div>
+          <div className="letter-box" style={this.state.letterStates[0][1]}>{this.state.row1[1]}</div>
+          <div className="letter-box" style={this.state.letterStates[0][2]}>{this.state.row1[2]}</div>
+          <div className="letter-box" style={this.state.letterStates[0][3]}>{this.state.row1[3]}</div>
+          <div className="letter-box" style={this.state.letterStates[0][4]}>{this.state.row1[4]}</div>
         </div>
 
         <div className="row">
-          <div className="letter-box">{this.state.row2[0]}</div>
-          <div className="letter-box">{this.state.row2[1]}</div>
-          <div className="letter-box">{this.state.row2[2]}</div>
-          <div className="letter-box">{this.state.row2[3]}</div>
-          <div className="letter-box">{this.state.row2[4]}</div>
+          <div className="letter-box" style={this.state.letterStates[1][0]}>{this.state.row2[0]}</div>
+          <div className="letter-box" style={this.state.letterStates[1][1]}>{this.state.row2[1]}</div>
+          <div className="letter-box" style={this.state.letterStates[1][2]}>{this.state.row2[2]}</div>
+          <div className="letter-box" style={this.state.letterStates[1][3]}>{this.state.row2[3]}</div>
+          <div className="letter-box" style={this.state.letterStates[1][4]}>{this.state.row2[4]}</div>
         </div>
 
       </div>
